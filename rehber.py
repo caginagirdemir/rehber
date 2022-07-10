eaaf178ce525f35a92034b37de44108ba047a8f8
Cagin Agirdemir <caginagirdemir@gmail.com>
2022-07-11 00:08:20 +0300

    Add files via upload

---
+++ rehber.py
@@ -0,0 +1,27 @@
+import sys
+from pydoc import pager
+import os
+
+def check_file(F_NAME):
+    os.chdir("/home/" + os.getenv('USER') + "/data/")
+    obj = os.scandir(".")
+    for entry in obj :
+        if (entry.is_file() & entry.name.endswith(".txt")):
+            with open(entry) as f:
+                if(entry.name == F_NAME+'.txt'):
+                    contents = f.read()
+                    pager(contents)
+                    return
+    print("Parametre Bulunamadi!")
+ 
+
+os.chdir("/home/" + os.getenv('USER'))
+os.system("git pull https://github.com/caginagirdemir/rehber")
+if(len(sys.argv) == 1):
+    check_file("yardim")
+elif(len(sys.argv) == 2):
+    if (sys.argv[1].lower() == "rehber"):
+        print("yardim")
+        check_file("yardim")
+    else:
+        check_file(sys.argv[1].lower())
